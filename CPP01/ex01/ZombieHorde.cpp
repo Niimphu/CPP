b1e8fff5480560c9fb1c84dd28a99e26e09ea196
Niimphu <nimphu@outlook.com>
2023-10-21 14:31:02 +0200

    heap zombies?

--- CPP01/ex01/ZombieHorde.cpp
+++ CPP01/ex01/ZombieHorde.cpp
@@ -3,7 +3,7 @@
 Zombie*	zombieHorde( int N, std::string name) {
 	Zombie*	zombieHorde = new Zombie[N];
 	for (int i = 0; i < N; ++i) {
-		zombieHorde[i] = Zombie(name);
+		new (&zombieHorde[i]) Zombie(name);
 	}
 	return zombieHorde;
 }
