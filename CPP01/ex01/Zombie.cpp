8496cd15a0d2bb431fbd356afb948bd2cc5f9c6b
Niimphu <nimphu@outlook.com>
2023-10-21 14:38:29 +0200

    simple main and moved announce

--- CPP01/ex01/Zombie.cpp
+++ CPP01/ex01/Zombie.cpp
@@ -7,3 +7,6 @@ Zombie::Zombie	( std::string name ) : _name(name) {}
 Zombie::~Zombie( void ) {
 	std::cout << _name << " is kill" << std::endl;
 }
+void	Zombie::announce() {
+	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
--- CPP01/ex01/main.cpp
+++ CPP01/ex01/main.cpp
@@ -3,14 +3,17 @@
 int	main( void ) {
 	int	N = 3;
 	Zombie* horde = zombieHorde(N, "Timothy");
-
 	for (int i = 0; i < N; ++i) {
 		horde[i].announce();
 	}
 
+	int M = 8;
+	Zombie* horde2 = zombieHorde(M, "Mary");
+	for (int i = 0; i < M; ++i) {
+		horde2[i].announce();
+	}
+
 	delete[] horde;
+	delete[] horde2;
 }
 
-void	Zombie::announce() {
-	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
-}
