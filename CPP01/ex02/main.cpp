d769856549f54dec628a77940c5f39fb24fe8b3c
yiwong <yiwong@student.42wolfsburg.de>
2023-10-21 16:26:50 +0200

    hi this is brain

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int	main( void ) {
+	std::string		string = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &string;
+	std::string&	stringREF = string;
+
+	std::cout << "Memory address of string: " << &string << std::endl;
+	std::cout << "Memory address of stringPTR: " << &stringPTR << std::endl;
+	std::cout << "Memory address of stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of string: " << string << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+}
