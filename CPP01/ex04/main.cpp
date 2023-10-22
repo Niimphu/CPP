4c63896fdb8e04e56e8e2b788dd314dfe28a83c4
yiwong <yiwong@student.42wolfsburg.de>
2023-10-22 13:55:38 +0200

    sed is for losers

---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+std::string	replace( std::string line, const std::string& s1, const std::string& s2 );
+
+int	main( int argc, char** argv ) {
+	if (argc != 4) {
+		std::cerr << "Invalid number of arguments: expected syntax: ./loser <filename> <string1> <string2>"
+				  << std::endl;
+		return 1;
+	}
+
+	const std::string	inputFilename = argv[1];
+	std::ifstream		inputFile(inputFilename);
+	if (!inputFile.is_open()) {
+		std::cerr << "Unable to open file " << inputFilename << std::endl;
+		return (1);
+	}
+
+	const std::string	outputFilename = inputFilename + ".replace";
+	std::ofstream		outputFile(outputFilename);
+	if (!inputFile.is_open()) {
+		std::cerr << "Unable to create file " << outputFilename << std::endl;
+		inputFile.close();
+		return (1);
+	}
+
+	std::string	s1 = argv[2];
+	std::string	s2 = argv[3];
+	std::string	line;
+	while (std::getline(inputFile, line)) {
+		outputFile << replace(line, s1, s2) << std::endl;
+	}
+
+	inputFile.close();
+	outputFile.close();
+	return 0;
+}
+
+std::string	replace( std::string line, const std::string& s1, const std::string& s2 ) {
+	size_t		pos = 0;
+	while ((pos = line.find(s1, pos)) != std::string::npos) {
+		line = line.substr(0, pos) + s2 + line.substr(pos + s1.length());
+		pos += s2.length();
+	}
+	return line;
+}
---
+++ CPP01/ex04/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+url="https://gist.githubusercontent.com/MattIPv4/045239bc27b16b2bcf7a3a9a4648c08a/raw/2411e31293a35f3e565f61e7490a806d4720ea7e/bee%2520movie%2520script"
+
+curl -s "$url" > example
+
+if [ $? -eq 0 ]; then
+    echo "Web page content fetched successfully and saved to 'example'."
+else
+    echo "Failed to fetch web page content."
+fi
+
+make
+
+./loser example bee "FUCKING LOSER"
+
+echo "Output saved to 'example.test'"
