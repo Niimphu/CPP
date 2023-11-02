24919081c3546919d2201a3563560e8c4b26ede5
yiwong <yiwong@student.42wolfsburg.de>
2023-11-02 12:56:01 +0100

    tidy up

--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -3,14 +3,15 @@
 int	main( void ) {
 	ClapTrap randall( "Randall" );
 	randall.setAttackDamage( 3 );
-	std::cout << "Stats: " << std::endl;
+	randall.setEnergyPoints( 4 );
+	std::cout << randall.getName() << "'s stats" << std::endl;
 	std::cout << "Hitpoints: " << randall.getHitPoints() << std::endl;
 	std::cout << "Energy points: " << randall.getEnergyPoints() << std::endl;
 	std::cout << "Attack damage: " << randall.getAttackDamage() << std::endl;
 	std::cout << std::endl;
 
 	ClapTrap bertha( "Bertha" );
-	std::cout << "Stats: " << std::endl;
+	std::cout << bertha.getName() << "'s stats" << std::endl;
 	std::cout << "Hitpoints: " << bertha.getHitPoints() << std::endl;
 	std::cout << "Energy points: " << bertha.getEnergyPoints() << std::endl;
 	std::cout << "Attack damage: " << bertha.getAttackDamage() << std::endl;
@@ -37,6 +38,9 @@ int	main( void ) {
 
 	randall.attack( bertha.getName() );
 	bertha.takeDamage( randall.getAttackDamage() );
+	std::cout << std::endl;
+
+	randall.attack( bertha.getName() );
 	std::cout << std::endl;
 
 	bertha.attack( randall.getName() );
