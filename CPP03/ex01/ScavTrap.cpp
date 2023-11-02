4cd2835a37715e405e3223a42fcce757a0708b52
yiwong <yiwong@student.42wolfsburg.de>
2023-11-02 16:29:03 +0100

    tidy up

--- CPP03/ex01/ScavTrap.cpp
+++ CPP03/ex01/ScavTrap.cpp
@@ -13,10 +13,10 @@ void	ScavTrap::attack( const std::string& target ) {
 		std::cout << "ScavTrap " << getName() << " does not have enough energy to attack" << std::endl;
 		return ;
 	}
-		if ( getHitPoints() == 0 ) {
-			std::cout << "ScavTrap " << getName() << " is dead and cannot attack!" << std::endl;
-			return ;
-		}
+	if ( getHitPoints() == 0 ) {
+		std::cout << "ScavTrap " << getName() << " is dead and cannot attack!" << std::endl;
+		return ;
+	}
 	std::cout << "ScavTrap " << getName() << " attacks " << target << " for " << getAttackDamage() << " damage" << std::endl;
 	setEnergyPoints( getEnergyPoints() - 1 );
 	return ;
