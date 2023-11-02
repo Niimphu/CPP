6053aa60fb73adc92ca0812182000758a4c1917e
yiwong <yiwong@student.42wolfsburg.de>
2023-11-02 16:21:22 +0100

    test main fin

--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -1,15 +1,30 @@
 #include "ScavTrap.hpp"
 
 int	main( void ) {
-	ScavTrap	bartholemew( "Bartholemew" );
-	std::cout << bartholemew.getName() << "'s stats" << std::endl;
-	std::cout << "Hitpoints: " << bartholemew.getHitPoints() << std::endl;
-	std::cout << "Energy points: " << bartholemew.getEnergyPoints() << std::endl;
-	std::cout << "Attack damage: " << bartholemew.getAttackDamage() << std::endl;
+	ScavTrap	serena( "Serena" );
+	std::cout << serena.getName() << "'s stats" << std::endl;
+	std::cout << "Hitpoints: " << serena.getHitPoints() << std::endl;
+	std::cout << "Energy points: " << serena.getEnergyPoints() << std::endl;
+	std::cout << "Attack damage: " << serena.getAttackDamage() << std::endl;
 	std::cout << std::endl;
 
-	bartholemew.attack( "Jeremiah");
-	bartholemew.guardGate();
+	ClapTrap	balthazar( "Balthazar" );
+	balthazar.setAttackDamage( 42 );
+	std::cout << balthazar.getName() << "'s stats" << std::endl;
+	std::cout << "Hitpoints: " << balthazar.getHitPoints() << std::endl;
+	std::cout << "Energy points: " << balthazar.getEnergyPoints() << std::endl;
+	std::cout << "Attack damage: " << balthazar.getAttackDamage() << std::endl;
+	std::cout << std::endl;
+
+	serena.attack( balthazar.getName() );
+	balthazar.takeDamage( serena.getAttackDamage() );
+	std::cout << std::endl;
+
+	balthazar.attack( serena.getName() );
+	balthazar.beRepaired( 2 );
+	balthazar.attack( serena.getName() );
+	serena.takeDamage( balthazar.getAttackDamage() );
+	serena.guardGate();
 	std::cout << std::endl;
 
 	return 0;
