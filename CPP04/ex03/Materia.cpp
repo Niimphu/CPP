d77fc0253bf7268c75921d25925228a67c510291
yiwong <yiwong@student.42wolfsburg.de>
2023-12-08 19:41:16 +0100

    improved output messages

--- CPP04/ex03/Materia.cpp
+++ CPP04/ex03/Materia.cpp
@@ -3,11 +3,11 @@
 AMateria::AMateria( void ) {}
 
 AMateria::AMateria( const std::string& type ) : _type( type ) {
-	std::cout << "Materia " << _type << " spawned" << std::endl;
+	std::cout << _type  " Materia spawned" << std::endl;
 }
 
 AMateria::~AMateria( void ) {
-	std::cout << "Materia " << _type << " destroyed" << std::endl;
+	std::cout << _type  " Materia destroyed" << std::endl;
 }
 
 void	AMateria::setType( const std::string& type ){
