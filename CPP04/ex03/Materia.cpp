c3e25d9ee6fbab481309a456ef4c66089a0861de
yiwong <yiwong@student.42wolfsburg.de>
2023-12-08 19:54:51 +0100

    added missing <<

--- CPP04/ex03/Materia.cpp
+++ CPP04/ex03/Materia.cpp
@@ -3,11 +3,11 @@
 AMateria::AMateria( void ) {}
 
 AMateria::AMateria( const std::string& type ) : _type( type ) {
-	std::cout << _type  " Materia spawned" << std::endl;
+	std::cout << _type << " Materia spawned" << std::endl;
 }
 
 AMateria::~AMateria( void ) {
-	std::cout << _type  " Materia destroyed" << std::endl;
+	std::cout << _type << " Materia destroyed" << std::endl;
 }
 
 void	AMateria::setType( const std::string& type ){
