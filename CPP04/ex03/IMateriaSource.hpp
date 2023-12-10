e01ec2af3df014cf7ed16cd4df5f1825adeed9b6
yiwong <yiwong@student.42wolfsburg.de>
2023-12-10 17:20:24 +0100

    materia source interface class added

--- CPP04/ex03/IMateriaSource.hpp
+++ CPP04/ex03/IMateriaSource.hpp
@@ -1,12 +1,12 @@
-#ifndef IMATERIA_H
-# define IMATERIA_H
+#ifndef IMATERIASOURCE_H
+# define IMATERIASOURCE_H
 
 #include "AMateria.hpp"
 
 class IMateriaSource
 {
 public:
-	virtual	~IMateriaSource() {}
+	virtual	~IMateriaSource( void ) {}
 
 	virtual void		learnMateria( AMateria* ) = 0;
 	virtual AMateria*	createMateria( const std::string& ) = 0;
