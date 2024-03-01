2c54166f40b57938af03442dbf202b82ef9ccc76
Niimphu <nimphu@outlook.com>
2024-03-01 14:10:05 +0100

    init forms

--- CPP05/ex01/Form.hpp
+++ CPP05/ex01/Form.hpp
@@ -6,17 +6,37 @@
 # include <exception>
 
 class Form {
-	public:
+public:
 	Form( void );
 	Form ( Form& );
 	~Form( void );
 
 	Form	&operator=( Form& );
 
-	private:
+	class GradeTooHighException : public std::exception {
+	public:
+		const char* what() const throw() {
+			return "Grade too high.";
+		}
+	};
 
+	class GradeTooLowException : public std::exception {
+	public:
+		const char* what() const throw() {
+			return "Grade too low.";
+		}
+	};
+
+
+private:
+	const std::string	_name;
+	bool				_isSigned;
+	const int			_gradeToSign;
+	const int			_gradeToExecute;
 
 
 };
+
+std::ostream&	operator<<( std::ostream&, const Form& );
 
 #endif
