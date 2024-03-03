bfe042665f8f425279d1acebe7ebfcf3782e1f5c
Niimphu <nimphu@outlook.com>
2024-03-03 21:51:32 +0100

    test main for shrubbos

--- CPP05/ex02/main.cpp
+++ CPP05/ex02/main.cpp
@@ -1,13 +1,19 @@
 #include "Bureaucrat.hpp"
-#include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 using std::cout;
 using std::endl;
 using std::cerr;
 
 int	main( void ) {
-	Bureaucrat	Daisy("Daisy", 130);
-	cout << Daisy << endl;
-	Bureaucrat	Berty("Berty", 130);
-	cout << Berty << endl;
+	try {
+		Bureaucrat Daisy("Daisy", 142);
+		cout << Daisy << endl;
+		ShrubberyCreationForm test = ShrubberyCreationForm();
+		Daisy.signForm( test );
+		Daisy.executeForm( test );
+	}
+	catch ( std::exception& e ) {
+		cerr << e.what() << endl;
+	}
 }
