73310c48782215b847bab43c2b6d53da5f3fefeb
Niimphu <nimphu@outlook.com>
2024-03-04 18:23:52 +0100

    robotomy test

--- CPP05/ex02/main.cpp
+++ CPP05/ex02/main.cpp
@@ -1,5 +1,6 @@
 #include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
 using std::cout;
 using std::endl;
@@ -7,11 +8,16 @@ using std::cerr;
 
 int	main( void ) {
 	try {
-		Bureaucrat Daisy("Daisy", 142);
+		Bureaucrat Daisy("Daisy", 20);
 		cout << Daisy << endl;
 		ShrubberyCreationForm test = ShrubberyCreationForm();
 		Daisy.signForm( test );
 		Daisy.executeForm( test );
+
+		RobotomyRequestForm	test2 = RobotomyRequestForm( "Tony" );
+		Daisy.signForm( test2 );
+		Daisy.executeForm( test2 );
+
 	}
 	catch ( std::exception& e ) {
 		cerr << e.what() << endl;
