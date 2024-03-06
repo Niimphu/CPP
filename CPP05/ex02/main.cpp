5717a77325555d565be859c8def6447ccffcf888
Niimphu <nimphu@outlook.com>
2024-03-06 14:46:19 +0100

    test main

--- CPP05/ex02/main.cpp
+++ CPP05/ex02/main.cpp
@@ -1,25 +1,66 @@
 #include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 using std::cout;
 using std::endl;
 using std::cerr;
 
 int	main( void ) {
+	Bureaucrat daisy("Daisy", 130);
+	cout << daisy << endl;
+
+	Bureaucrat tony("Tony", 28);
+	cout << tony << endl;
+
+	Bureaucrat mary("Mary", 2);
+	cout << mary << endl;
+	cout << endl;
+
 	try {
-		Bureaucrat Daisy("Daisy", 20);
-		cout << Daisy << endl;
-		ShrubberyCreationForm test = ShrubberyCreationForm();
-		Daisy.signForm( test );
-		Daisy.executeForm( test );
-
-		RobotomyRequestForm	test2 = RobotomyRequestForm( "Tony" );
-		Daisy.signForm( test2 );
-		Daisy.executeForm( test2 );
-
+		ShrubberyCreationForm form = ShrubberyCreationForm( "Park" );
+		daisy.signForm( form );
+		daisy.executeForm( form );
 	}
 	catch ( std::exception& e ) {
 		cerr << e.what() << endl;
 	}
+	cout << endl;
+
+	try {
+		RobotomyRequestForm	form = RobotomyRequestForm( daisy.getName() );
+		tony.signForm( form );
+		tony.executeForm( form );
+	}
+	catch ( std::exception& e ) {
+		cerr << e.what() << endl;
+	}
+	cout << endl;
+
+	try {
+		PresidentialPardonForm	form = PresidentialPardonForm( tony.getName() );
+		tony.executeForm( form );
+	}
+	catch ( std::exception& e ) {
+		cerr << e.what() << endl;
+	}
+	try {
+		PresidentialPardonForm	form = PresidentialPardonForm( tony.getName() );
+		tony.signForm( form );
+	}
+	catch ( std::exception& e ) {
+		cerr << e.what() << endl;
+	}
+	cout << endl;
+
+	try {
+		PresidentialPardonForm	form = PresidentialPardonForm( tony.getName() );
+		mary.signForm( form );
+		mary.executeForm( form );
+	}
+	catch ( std::exception& e ) {
+		cerr << e.what() << endl;
+	}
+	cout << endl;
 }
