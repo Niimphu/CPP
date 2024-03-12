cb86ebc52c2acec69793483039e1be66a35ff464
Niimphu <nimphu@outlook.com>
2024-03-12 19:20:22 +0100

    struct for types

--- CPP06/ex00/Type.hpp
+++ CPP06/ex00/Type.hpp
@@ -10,8 +10,17 @@ enum Type {
 	DOUBLE,
 	PSEUDO_FLOAT,
 	PSEUDO_DOUBLE,
+	NUMBER,
 	UNKNOWN
 };
+
+typedef struct	s_set {
+	char	c;
+	int		i;
+	float	f;
+	double	d;
+	Type	type;
+}				t_set;
 
 const std::string	P_FLOATS[] = { "-inff", "+inff", "nanf" };
 const std::string	P_DOUBLES[] = { "-inf", "+inf", "nan" };
