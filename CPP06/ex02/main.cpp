e235a613b2f187b56869f837b14f815c0b6334d5
Niimphu <nimphu@outlook.com>
2024-03-14 00:41:39 +0100

    generate() implementation is working

--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -3,23 +3,54 @@
 #include "B.hpp"
 #include "C.hpp"
 #include <iostream>
+#include <string>
+#include <stdexcept>
 
 Base*	generate(void);
-void	identify(Base p);
-void	identify(Base p);
+void	identify(Base*);
+void	identify(Base&);
 
 int	main(void) {
-
+	std::srand(static_cast<unsigned int>(std::time(0)));
+	Base*	randomInstance;
+	try {
+		randomInstance = generate();
+	}
+	catch (const std::string e) {
+		std::cerr << e << std::endl;
+	}
+	delete randomInstance;
 }
 
 Base*	generate(void) {
-
+	Base*	instance;
+    int	rng = std::rand() % 3 + 1;
+	switch(rng) {
+		case 1: {
+			instance = new A();
+			std::cout << "Class A was generated" << std::endl;
+			break;
+		}
+		case 2: {
+			instance = new B();
+			std::cout << "Class B was generated" << std::endl;
+			break;
+		}
+		case 3: {
+			instance = new C();
+			std::cout << "Class C was generated" << std::endl;
+			break;
+		}
+		default: {
+			throw std::string("Error generating class");
+		}
+	}
+	return instance;
 }
 
-void	identify(Base* p) {
-
+void	identify(Base*) {
 }
 
-void	identify(Base& p) {
+void	identify(Base&) {
 
 }
