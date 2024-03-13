b35830a269ccb3c0efcd3ef55f2ec8494969ca9b
Niimphu <nimphu@outlook.com>
2024-03-13 19:11:09 +0100

    init main

---
+++ CPP06/ex02/main.cpp
@@ -0,0 +1,25 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+
+Base*	generate(void);
+void	identify(Base p);
+void	identify(Base p);
+
+int	main(void) {
+
+}
+
+Base*	generate(void) {
+
+}
+
+void	identify(Base* p) {
+
+}
+
+void	identify(Base& p) {
+
+}
