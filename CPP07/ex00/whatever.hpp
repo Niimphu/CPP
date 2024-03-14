205ea842bb69399f908975c84da851d9f9997c97
Niimphu <nimphu@outlook.com>
2024-03-14 19:05:48 +0100

    moved function templates to header file

--- CPP07/ex00/whatever.hpp
+++ CPP07/ex00/whatever.hpp
@@ -1,12 +1,25 @@
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
 
+template<typename T>
+void	swap(T& a, T& b) {
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
 
 template<typename T>
-void	swap(T&, T&);
+T&	min(T& a, T& b) {
+	if (a < b)
+		return a;
+	return b;
+}
+
 template<typename T>
-T&		min(T&, T&);
-template<typename T>
-T&		max(T&, T&);
+T&	max(T& a, T& b) {
+	if (a > b)
+		return a;
+	return b;
+}
 
 #endif
