4343cf09c8afe791c986beec884397bac66f1161
Niimphu <nimphu@outlook.com>
2024-03-14 19:04:17 +0100

    basic implementation done

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -1,6 +1,5 @@
 #include "whatever.hpp"
 #include <iostream>
-#include <string>
 
 int	main(void) {
 	int a = 2;
@@ -18,8 +17,23 @@ int	main(void) {
 	return 0;
 }
 
-void	swap(int &a, int &b) {
-	int tmp = a;
+template<typename T>
+void	swap(T& a, T& b) {
+	T tmp = a;
 	a = b;
 	b = tmp;
+}
+
+template<typename T>
+T&	min(T& a, T& b) {
+	if (a < b)
+		return a;
+	return b;
+}
+
+template<typename T>
+T&	max(T& a, T& b) {
+	if (a > b)
+		return a;
+	return b;
 }
--- CPP07/ex00/whatever.hpp
+++ CPP07/ex00/whatever.hpp
@@ -1,6 +1,12 @@
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
 
-# include ""
+
+template<typename T>
+void	swap(T&, T&);
+template<typename T>
+T&		min(T&, T&);
+template<typename T>
+T&		max(T&, T&);
 
 #endif
