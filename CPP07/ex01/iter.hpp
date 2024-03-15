3461480b274c505c135c2c6273713864ef0ccf24
Niimphu <nimphu@outlook.com>
2024-03-15 14:21:30 +0100

    included header for size_t

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -1,6 +1,8 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
+#include <cstddef>
+
 template<typename T, typename F>
 void	iter(T* array, size_t length, F function) {
 	for (size_t i = 0; i < length; i++) {
