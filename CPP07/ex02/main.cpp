9ad0e92a749fa8fa432bd9c5a86b6761afb25f95
Niimphu <nimphu@outlook.com>
2024-03-16 22:13:28 +0100

    updated tests to use print()

--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -5,9 +5,9 @@ int	main(void) {
 	Array<std::string>	strArray(4);
 	for (size_t i = 0; i < 4; i++) {
 		strArray[i] = "Pope";
-		std::cout << "Index: " << i << " Value: " << strArray[i] << std::endl;
 	}
-	std::cout << std::endl << std::endl;
+	strArray.print();
+	std::cout << std::endl;
 
 	Array<std::string>	strArrayCopy;
 	strArrayCopy = strArray;
@@ -16,16 +16,13 @@ int	main(void) {
 		ss << (i + 1);
 		strArrayCopy[i] += ss.str();
 	}
-	for (size_t i = 0; i < 4; i++) {
-		std::cout << "Index: " << i << " Value: " << strArray[i] << std::endl;
-	}
-	for (size_t i = 0; i < 4; i++) {
-		std::cout << "Index: " << i << " Value: " << strArrayCopy[i] << std::endl;
-	}
-	std::cout << std::endl << std::endl;
+	strArray.print();
+	strArrayCopy.print();
+	std::cout << std::endl;
 
 	try {
-		std::cout << strArray[7] << std::endl;
+		std::cout << strArrayCopy[2] << std::endl;
+		std::cout << strArrayCopy[7] << std::endl;
 	}
 	catch (const std::exception& e) {
 		std::cerr << e.what() << std::endl;
