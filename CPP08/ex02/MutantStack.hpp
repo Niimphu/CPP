de00d68dec3ebe794ac220cb340936132928905e
Niimphu <nimphu@outlook.com>
2024-03-18 18:57:02 +0100

    abom stack implementation

---
+++ CPP08/ex02/MutantStack.hpp
@@ -0,0 +1,33 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+
+template<typename T>
+class MutantStack : public std::stack<T> {
+public:
+	typedef typename std::stack<T>::container_type::iterator iterator;
+
+	MutantStack(void): std::stack<T>() {}
+	MutantStack(const MutantStack &original): std::stack<T>(original) {}
+	MutantStack &operator=(const MutantStack& other) {
+		if (this != &other)
+			std::stack<T>::operator=(other);
+		return *this;
+	}
+    T&  operator[](size_t index) {
+        iterator it = this->begin();
+        std::advance(it, index);
+        return *it;
+    }
+	~MutantStack(void) {}
+
+	iterator begin(void) {
+		return this->c.begin();
+	}
+	iterator end(void) {
+		return this->c.end();
+	}
+};
+
+#endif
