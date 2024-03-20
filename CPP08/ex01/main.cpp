ccf92bc63efbd097f7b33519abf8bc75c7e66b91
Niimphu <nimphu@outlook.com>
2024-03-20 17:57:12 +0100

    more tests for new functioN

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -30,9 +30,20 @@ int	main(void) {
 	catch (const std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
+	std::cout << std::endl;
+
+	Span	test2 = Span(20);
+	try {
+		test2.addRange(42, 61);
+		std::cout << test2.shortestSpan() << std::endl;
+		std::cout << test2.longestSpan() << std::endl;
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
 
 	try {
-		test.addNumber(17);
+		test2.addNumber(42);
 	}
 	catch (const std::exception& e) {
 		std::cerr << e.what() << std::endl;
