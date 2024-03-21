867859995f9ffd93c25791559174ec9abd9c6355
Niimphu <nimphu@outlook.com>
2024-03-21 16:28:03 +0100

    minitest

--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -1,5 +1,10 @@
 #include "BitcoinExchange.hpp"
 
 int	main(void) {
+	BitcoinExchange	btc;
+	std::map<std::string, float>	database = btc.getDatabase();
+	std::map<std::string, float>::const_iterator it = database.begin();
+	std::advance(it, 200);
+	std::cout << it->second << std::endl;
 	return 0;
 }
